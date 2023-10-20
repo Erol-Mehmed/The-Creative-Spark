33b50cf04fb89cffaab39e3b4c1ad4aee358088a
Erol-web <erol.mehmed07@gmail.com>
2023-10-21 00:11:59 +0300

    blog: backend restart with django

--- backend/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authController = require('../controllers/authController');
-
-const router = express.Router();
-
-module.exports = router;
-
-router.post('/register', authController.registerUser);
-router.post('/login', authController.loginUser);
