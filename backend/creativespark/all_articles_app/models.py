b48cc645ec9a3913ac156fd495410d8b29434c4f
Erol-web <erol.mehmed07@gmail.com>
2023-12-08 22:04:13 +0200

    blog: topics progress

--- backend/creativespark/all_articles_app/models.py
+++ backend/creativespark/all_articles_app/models.py
@@ -6,6 +6,7 @@ class Article(models.Model):
     author = models.ForeignKey(User, on_delete=models.PROTECT)
     title = models.CharField(max_length=500, default=None)
     content = models.TextField(default=None)
+    topic = models.CharField(max_length=100, default=None)
     image = models.ImageField(upload_to='article_images/')
     claps = models.IntegerField(default=0)
     read_time = models.IntegerField(default=0)
