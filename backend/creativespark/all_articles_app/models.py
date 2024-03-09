a3620086035af4f9b5956d7b109a07924899cbce
Erol-web <erol.mehmed07@gmail.com>
2024-03-09 21:06:34 +0200

    blog: article page progress

--- backend/creativespark/all_articles_app/models.py
+++ backend/creativespark/all_articles_app/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from users_app.models import User
+from autoslug import AutoSlugField
 
 
 class Article(models.Model):
     author = models.ForeignKey(User, on_delete=models.PROTECT)
     title = models.CharField(max_length=500, default=None)
+    slug = AutoSlugField(populate_from="title")
     content = models.TextField(default=None)
     topic = models.CharField(max_length=100, default=None)
     image = models.ImageField(upload_to="article_images/")
