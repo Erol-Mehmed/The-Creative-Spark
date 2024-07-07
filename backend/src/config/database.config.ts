15ce788e456382182e08e67b4c2c552b1836ad48
Erol Mehmed <erol.mehmed07@gmail.com>
2024-07-07 21:46:44 +0300

    blog: nestjs backend progress

---
+++ backend/src/config/database.config.ts
@@ -0,0 +1,8 @@
+import * as process from "node:process";
+
+export const databaseConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
+  const appPort = process.env.APP_PORT;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(appPort);
+
+  console.log(`Server running on http://localhost:${appPort}`);
 }
+
 bootstrap();
