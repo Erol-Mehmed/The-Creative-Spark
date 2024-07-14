9358804b09c73d50d3cd2e089cc56a246361de55
Erol Mehmed <erol.mehmed07@gmail.com>
2024-07-15 00:29:24 +0300

    blog: backend refactor progress

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,17 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
+      host: 'localhost',
+      port: 5432,
       username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [],
+      // username: 'postgres',
+      // password: String(process.env.DB_PASSWORD),
+      password: '123456',
+      database: 'creative_spark',
+      entities: [User],
       synchronize: true,
     }),
   ],
